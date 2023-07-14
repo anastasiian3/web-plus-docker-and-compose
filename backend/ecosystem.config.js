f20edd7c03aa08f5c9efb458bf8031d66568e0a6
Anastasiya N <anastasa3n@yandex.ru>
2023-07-14 22:49:03 +0300

    fix: readme updated

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,30 +1,8 @@
-require('dotenv').config({ path: './.env.deploy' });
-
-const {
-  DEPLOY_USER,
-  DEPLOY_HOST,
-  DEPLOY_PATH,
-  DEPLOY_REF = 'origin/main',
-  DEPLOY_REPO,
-} = process.env;
-
 module.exports = {
   apps: [
     {
-      name: 'kpd',
+      name: 'backend',
       script: './dist/main.js',
     },
   ],
-  deploy: {
-    production: {
-      user: DEPLOY_USER,
-      host: DEPLOY_HOST,
-      ref: DEPLOY_REF,
-      repo: DEPLOY_REPO,
-      path: DEPLOY_PATH,
-      'pre-deploy-local': `bash scripts/deployEnv.sh ${DEPLOY_USER}@${DEPLOY_HOST} ${DEPLOY_PATH}`,
-      'post-deploy':
-        'cd backend && pwd && npm i && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
-    },
-  },
 };
